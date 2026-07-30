7f1edc259bb34917de15814bbfb9d411d181a1e5
unbot2313 <tomasramirez0601@gmail.com>
2026-07-30 09:58:00 -0500

    fix: templates as workshop spec

--- movie/views.py
+++ movie/views.py
@@ -3,17 +3,18 @@ from django.shortcuts import render
 from .models import Movie
 
 
+# Create your views here.
+
 def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    #return render(request, 'home.html', {'name':'Tomas Ramirez Galeano'})
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {
-        'name': 'Tomas Ramirez Galeano',
-        'searchTerm': searchTerm,
-        'movies': movies,
-    })
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies, 'name':'Tomas Ramirez Galeano'})
 
 
 def about(request):
