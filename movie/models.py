23e5b2958a09bd8c76d9097345d559b9b2986d09
unbot2313 <tomasramirez0601@gmail.com>
2026-07-30 09:45:26 -0500

    feature: movie model and admin

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
