66c47ff55b87f39bd13c161fee549fd4a78af34b
unbot2313 <tomasramirez0601@gmail.com>
2026-08-20 10:14:28 -0500

    creacion de app news

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from movie import views as movieViews
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
+    path('news/', include('news.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
