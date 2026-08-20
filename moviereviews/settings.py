52af4dbc9ec3455f3a644199261848aa334a64cc
unbot2313 <tomasramirez0601@gmail.com>
2026-08-20 10:35:14 -0500

    navbar y footer

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'moviereviews/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [BASE_DIR / 'moviereviews' / 'static']
